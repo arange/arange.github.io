53e44510b4526cac4a70ac4ef31c4a936788fb4f
Allen <panpan2558@gmail.com>
2019-05-25 02:48:14 +1000

    Update bill-spliter.js

--- js/bill-spliter.js
+++ js/bill-spliter.js
@@ -2,15 +2,14 @@ var spliter = {
 
 	recordEl : '<i><input class="sharer" placeholder="Name of each people." /> share the bill for $<input class="aom" placeholder="Amount of money" />, <input class="payer" placeholder="Payer" /> paid for it.</i>',
 	recordNum : 0,
-	separator : [" ",","],
 
 	init : function() {
 		$("#calc").hide();
 	},
 
 	addNew : function() {
-		$("#recordBox").append(recordEl);
-		recordNum++;
+		$("#recordBox").append(spliter.recordEl);
+		spliter.recordNum++;
 	},
 
 
@@ -21,8 +20,8 @@ var spliter = {
 	calc : function() {
 		var people = {};
 
-		recordEls = $("#recordBox > i");
-		for (var i = 0; i < recordNum; i++) {
+		spliter.recordEls = $("#recordBox > i");
+		for (var i = 0; i < spliter.recordNum; i++) {
 			var peopleList = spliter.splitPeople(recordEls[i].children(".sharer").val());
 			var numOfSharer = peopleList.length;
 			var amount = parseInt(recordEls[i].children(".aom").val().match(/[0-9]+/));
