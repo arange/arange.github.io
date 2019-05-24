049f5504cce7f9ca0f830457c5d65e5db41f3303
Allen <panpan2558@gmail.com>
2019-05-25 02:46:24 +1000

    bill spliter function

--- js/bill-spliter.js
+++ js/bill-spliter.js
@@ -1,8 +1,56 @@
-var spliter={
+var spliter = {
 
-	init:function(){
-		console.log("test")
+	recordEl : '<i><input class="sharer" placeholder="Name of each people." /> share the bill for $<input class="aom" placeholder="Amount of money" />, <input class="payer" placeholder="Payer" /> paid for it.</i>',
+	recordNum : 0,
+	separator : [" ",","],
+
+	init : function() {
+		$("#calc").hide();
+	},
+
+	addNew : function() {
+		$("#recordBox").append(recordEl);
+		recordNum++;
+	},
+
+
+	showCalc : function() {
+		$("#calc").show();
+	},
+
+	calc : function() {
+		var people = {};
+
+		recordEls = $("#recordBox > i");
+		for (var i = 0; i < recordNum; i++) {
+			var peopleList = spliter.splitPeople(recordEls[i].children(".sharer").val());
+			var numOfSharer = peopleList.length;
+			var amount = parseInt(recordEls[i].children(".aom").val().match(/[0-9]+/));
+			if (isNaN(amount)){
+				alert("Please enter the amount of money correctly!");
+				break;
+			}
+			else {
+				var avg = amount / numOfSharer;
+			}
+			for (var j = 0; j < peopleList.length; j++) {
+				if (!people.peopleList[j]){
+					people.peopleList[j] = 0;
+				}
+				people.peopleList[j] += avg;
+			}
+			var payer = recordEls[i].children(".payer").val().trim();
+			if(!people.payer){
+				people.payer = 0;
+			}
+			people.payer -= avg;
+		}
+	},
+
+	splitPeople : function(str) {
+		return str.trim().split(/[\s,]+/);
 	}
+
 	
 };
 
